5f1e4511169d7397ef4f369e3c687914a1350473
Anna Sobanska <ania.webdev@gmail.com>
2021-06-26 11:42:13 +0200

    add file ios out

---
+++ Quiz/Main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+int main() {
+	std::string name, surName;
+	int mobileNumber;
+	std::cout << "Enter your name: ";
+	std::cin >> name;
+	std::cout << "Enter your surname: ";
+	std::cin >> surName;
+	std::cout << "Enter your mobile phone: ";
+	std::cin >> mobileNumber;
+
+	std::fstream file;
+	file.open("card.txt", std::ios::out);
+
+	file << name << std::endl;
+	file << surName << std::endl;
+	file << mobileNumber << std::endl;
+
+	file.close();
+
+	system("pause>0");
+}
